78a50edb953b4b35caae75f0b0a63170e19e9f85
大洞　日音 <HINARI@oohoranichion-no-MacBook-Air.local>
2018-11-29 14:15:44 +0900

    Add Level.class

--- LevelDesign/Level.java
+++ LevelDesign/Level.java
@@ -1,18 +1,14 @@
 public class Level{
-    private class Size{
-        int gridsize;
-        int width;
-        int height;
-    }
+
     String Name;
     String BackgroundName;
-    Size size;
+    LevelSize levelsize;
     Object[] objects;
 
     Level(){
-        size.gridsize=0;
-        size.width=0;
-        size.height=0;
+        levelsize.gridsize=0;
+        levelsize.width=0;
+        levelsize.height=0;
         Name="asdf";
         BackgroundName="NoImage";
     }
---
+++ LevelDesign/LevelSize.java
@@ -0,0 +1,5 @@
+public class LevelSize{
+    int gridsize;
+    int width;
+    int height;
+}
