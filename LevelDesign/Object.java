609dbb66c2ecab974e409ea6e6009b5d5396eed3
Nasa Matsumoto <labrador1592mary@gmail.com>
2018-11-29 14:25:16 +0900

    Add Object.class

---
+++ LevelDesign/Object.java
@@ -0,0 +1,17 @@
+public class Object
+{
+  private class Size{
+    int anchorx;
+    int anchory;
+  }
+  String Image;
+  String Name;
+  Size size;
+
+  Object(){
+    size.anchorx = 0;
+    size.anchory = 0;
+    Image="NoImage";
+    Name="asdf";
+  }
+}
