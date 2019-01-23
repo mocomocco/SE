5812ba191753938312061fd658e267233796dc29
Rekite <edward10379@gmail.com>
2019-01-23 15:08:03 +0900

    modified OpenFile

--- LevelDesign/src/main/java/OpFileDialogs.java
+++ LevelDesign/src/main/java/OpFileDialogs.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.io.*;
 import java.io.File;
+import javax.swing.filechooser.*;
 
 class OpFileDialogs extends JFrame
 {
 	JFileChooser fc;
+	String targetjson;
 
 	public OpFileDialogs()
 	{
@@ -13,9 +15,22 @@ class OpFileDialogs extends JFrame
 		} catch (Exception e) {} // setting Look And Feel
 		
 		fc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON Files", "json");
+		fc.setFileFilter(filter);
+		fc.setMultiSelectionEnabled(false);
+
 		int returnVal = fc.showOpenDialog(null);
 		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			File selectFile = fc.getSelectedFile(); //fix to get file
+			File selectFile = fc.getSelectedFile();
+			String filename = fc.getSelectedFile().getName();
+			String targetjson = selectFile.getAbsolutePath();
+			try{
+				new Display(filename, targetjson);
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		} else if(returnVal == JFileChooser.CANCEL_OPTION) {
+			JOptionPane.showMessageDialog(null, "File open has been canceled");
 		}
 	}
 }
